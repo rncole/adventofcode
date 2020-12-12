a36e214c4f8eb9bc59dabcce2c21fa0242673e65
Robert Cole <rncole@gmail.com>
2020-12-12 16:43:32 -0500

    Day 10 Part 1 and 2 complete!

--- 10/main.py
+++ 10/main.py
@@ -1,7 +1,9 @@
 # Advent of Code Day 10: Python Solution
 
 # Part 1:
-infile = open('input.txt','r')
+import functools
+
+infile = open('input.txt', 'r')
 adapters = []
 
 with infile as f:
@@ -10,18 +12,44 @@ with infile as f:
 
 adapters.append('0')
 adapters = list(map(int, adapters))
-adapters.sort()
 adapters.append(max(adapters) + 3)
-# print(adapters)
-# print(adapters[3])
+adapters.sort()
 
 adapters_1jolt = 0
 adapters_3jolt = 0
 
-for i in range(0,len(adapters)-1):
+adapters_with_diff = []
+
+for i in range(0, len(adapters)-1):
     if adapters[i+1] - adapters[i] == 1:
         adapters_1jolt = adapters_1jolt + 1
+        adapters_with_diff.append([adapters[i+1], 1])
     if adapters[i+1] - adapters[i] == 3:
         adapters_3jolt = adapters_3jolt + 1
+        adapters_with_diff.append([adapters[i+1], 3])
 print('1-Jolt Adapters:', adapters_1jolt, '\n3-Jolt Adapters:', adapters_3jolt)
 print('Solution:', adapters_1jolt * adapters_3jolt)
+
+seq_count = 0
+adapters_with_diff_seq = []
+
+for j in range(0, len(adapters_with_diff)-1):
+    if adapters_with_diff[j][1] == 1:
+        seq_count = seq_count + 1
+    if adapters_with_diff[j][1] == 3:
+        seq_count = 0
+    adapters_with_diff_seq.append([adapters_with_diff[j][0], adapters_with_diff[j][1], seq_count])
+
+combo_count = []
+for k in range(0, len(adapters_with_diff_seq)-1):
+    if int(adapters_with_diff_seq[k+1][2]) == 0 and int(adapters_with_diff_seq[k][2]) != 0:
+        if int(adapters_with_diff_seq[k][2]) == 1:
+            combo_count.append(1)
+        if int(adapters_with_diff_seq[k][2]) == 2:
+            combo_count.append(2)
+        if int(adapters_with_diff_seq[k][2]) == 3:
+            combo_count.append(4)
+        if int(adapters_with_diff_seq[k][2]) == 4:
+            combo_count.append(7)
+
+print('Part 2 Solution:', functools.reduce((lambda a, b: a*b), combo_count))
