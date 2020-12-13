cfeb5da80967b0e5bc2402f7272bd1b93a7a3b53
Robert Cole <rncole@gmail.com>
2020-12-13 13:39:49 -0500

    Day 12 Final Solution

--- 12/main.py
+++ 12/main.py
@@ -8,25 +8,16 @@ with infile as f:
         line = line.rstrip()
         ship_movement.append([line[slice(1)], int(line[slice(1, 5)])])
 
-# print(ship_movement)
-
 ship_position = [0, 0, 'E']
-# print(ship_position[2])
-# print(ship_movement[1][0])
-# print(ship_movement[1][1])
 turned = 0
 
 for i in range(0, len(ship_movement)):
     if ship_movement[i][0] == 'R':
-        # print('turning right')
         if ship_position[2] == 'E':
-            # print('facing east')
-            # print(ship_movement[i][1])
             if ship_movement[i][1] == 90:
                 if turned == 0:
                     turned = 1
                     ship_position = [ship_position[0], ship_position[1], 'S']
-                # print(ship_position)
             if ship_movement[i][1] == 180:
                 if turned == 0:
                     turned = 1
@@ -129,91 +120,42 @@ for i in range(0, len(ship_movement)):
                     ship_position = [ship_position[0], ship_position[1], 'E']
     if ship_movement[i][0] == 'F':
         if ship_position[2] == 'E':
-            ship_position = [int(ship_position[0])+int(ship_movement[i][1]), ship_position[1], ship_position[2]]
+            ship_position = [ship_position[0] + ship_movement[i][1], ship_position[1], ship_position[2]]
         if ship_position[2] == 'W':
-            ship_position = [int(ship_position[0])-int(ship_movement[i][1]), ship_position[1], ship_position[2]]
+            ship_position = [ship_position[0] - ship_movement[i][1], ship_position[1], ship_position[2]]
         if ship_position[2] == 'N':
-            ship_position = [ship_position[0], int(ship_position[1]) + int(ship_movement[i][1]), ship_position[2]]
+            ship_position = [ship_position[0], ship_position[1] + ship_movement[i][1], ship_position[2]]
         if ship_position[2] == 'S':
-            ship_position = [ship_position[0], int(ship_position[1]) - int(ship_movement[i][1]), ship_position[2]]
+            ship_position = [ship_position[0], ship_position[1] - ship_movement[i][1], ship_position[2]]
     if ship_movement[i][0] == 'N':
-        ship_position = [ship_position[0], int(ship_position[1]) + int(ship_movement[i][1]), ship_position[2]]
+        ship_position = [ship_position[0], ship_position[1] + ship_movement[i][1], ship_position[2]]
     if ship_movement[i][0] == 'S':
-        ship_position = [ship_position[0], int(ship_position[1]) - int(ship_movement[i][1]), ship_position[2]]
+        ship_position = [ship_position[0], ship_position[1] - ship_movement[i][1], ship_position[2]]
     if ship_movement[i][0] == 'E':
-        ship_position = [int(ship_position[0]) + int(ship_movement[i][1]), ship_position[1], ship_position[2]]
+        ship_position = [ship_position[0] + ship_movement[i][1], ship_position[1], ship_position[2]]
     if ship_movement[i][0] == 'W':
-        ship_position = [int(ship_position[0]) - int(ship_movement[i][1]), ship_position[1], ship_position[2]]
+        ship_position = [ship_position[0] - ship_movement[i][1], ship_position[1], ship_position[2]]
     turned = 0
-    # print(ship_position)
 
+print('\nPart 1 Solution:\n=================================')
 print('Final Position:', ship_position)
-
 print('Manhattan Distance:', abs(int(ship_position[0])) + abs(int(ship_position[1])))
 
-turned = 0
-i = 0
 ship_position = [0, 0, 'E']
 waypoint_position = [10, 1]
 
 for i in range(0, len(ship_movement)):
     if ship_movement[i][0] == 'R':
-        # print('turning right')
-        # print('ship:', ship_position)
-        # print('waypoint:', waypoint_position)
-        if ship_position[2] == 'E':
-            if ship_movement[i][1] == 90:
-                waypoint_offset = [waypoint_position[0]-ship_position[0], waypoint_position[1]-ship_position[1]]
-                waypoint_position = [(ship_position[0]+waypoint_offset[1]), (ship_position[1]-waypoint_offset[0])]
-            if ship_movement[i][1] == 180:
-                waypoint_offset = [waypoint_position[0]-ship_position[0], waypoint_position[1]-ship_position[1]]
-                waypoint_position = [(ship_position[0] - waypoint_offset[0]), (ship_position[1]-waypoint_offset[1])]
-            if ship_movement[i][1] == 270:
-                waypoint_offset = [waypoint_position[0]-ship_position[0], waypoint_position[1]-ship_position[1]]
-                waypoint_position = [(ship_position[0]-waypoint_offset[1]), (ship_position[1]+waypoint_offset[0])]
-            ship_position = [ship_position[0], ship_position[1], 'S']
-            turned = 1
-        if ship_position[2] == 'W':
-            if ship_movement[i][1] == 90:
-                waypoint_offset = [waypoint_position[0]-ship_position[0], waypoint_position[1]-ship_position[1]]
-                waypoint_position = [(ship_position[0]+waypoint_offset[1]), (ship_position[1]-waypoint_offset[0])]
-            if ship_movement[i][1] == 180:
-                waypoint_offset = [waypoint_position[0]-ship_position[0], waypoint_position[1]-ship_position[1]]
-                waypoint_position = [(ship_position[0] - waypoint_offset[0]), (ship_position[1]-waypoint_offset[1])]
-            if ship_movement[i][1] == 270:
-                waypoint_offset = [waypoint_position[0]-ship_position[0], waypoint_position[1]-ship_position[1]]
-                waypoint_position = [(ship_position[0]-waypoint_offset[1]), (ship_position[1]+waypoint_offset[0])]
-            ship_position = [ship_position[0], ship_position[1], 'N']
-            turned = 1
-        if ship_position[2] == 'N':
-            if ship_movement[i][1] == 90:
-                waypoint_offset = [waypoint_position[0] - ship_position[0], waypoint_position[1] - ship_position[1]]
-                waypoint_position = [(ship_position[0] + waypoint_offset[1]), (ship_position[1] - waypoint_offset[0])]
-            if ship_movement[i][1] == 180:
-                waypoint_offset = [waypoint_position[0] - ship_position[0], waypoint_position[1] - ship_position[1]]
-                waypoint_position = [(ship_position[0] - waypoint_offset[0]), (ship_position[1] - waypoint_offset[1])]
-            if ship_movement[i][1] == 270:
-                waypoint_offset = [waypoint_position[0] - ship_position[0], waypoint_position[1] - ship_position[1]]
-                waypoint_position = [(ship_position[0] - waypoint_offset[1]), (ship_position[1] + waypoint_offset[0])]
-            ship_position = [ship_position[0], ship_position[1], 'E']
-            turned = 1
-        if ship_position[2] == 'S':
-            if ship_movement[i][1] == 90:
-                waypoint_offset = [waypoint_position[0] - ship_position[0], waypoint_position[1] - ship_position[1]]
-                waypoint_position = [(ship_position[0] + waypoint_offset[1]), (ship_position[1] - waypoint_offset[0])]
-            if ship_movement[i][1] == 180:
-                waypoint_offset = [waypoint_position[0] - ship_position[0], waypoint_position[1] - ship_position[1]]
-                waypoint_position = [(ship_position[0] - waypoint_offset[0]), (ship_position[1] - waypoint_offset[1])]
-            if ship_movement[i][1] == 270:
-                waypoint_offset = [waypoint_position[0] - ship_position[0], waypoint_position[1] - ship_position[1]]
-                waypoint_position = [(ship_position[0] - waypoint_offset[1]), (ship_position[1] + waypoint_offset[0])]
-            ship_position = [ship_position[0], ship_position[1], 'W']
-            turned = 1
-        # print('ship:', ship_position)
-        # print('waypoint:', waypoint_position)
+        if ship_movement[i][1] == 90:
+            waypoint_offset = [waypoint_position[0]-ship_position[0], waypoint_position[1]-ship_position[1]]
+            waypoint_position = [(ship_position[0]+waypoint_offset[1]), (ship_position[1]-waypoint_offset[0])]
+        if ship_movement[i][1] == 180:
+            waypoint_offset = [waypoint_position[0]-ship_position[0], waypoint_position[1]-ship_position[1]]
+            waypoint_position = [(ship_position[0] - waypoint_offset[0]), (ship_position[1]-waypoint_offset[1])]
+        if ship_movement[i][1] == 270:
+            waypoint_offset = [waypoint_position[0]-ship_position[0], waypoint_position[1]-ship_position[1]]
+            waypoint_position = [(ship_position[0]-waypoint_offset[1]), (ship_position[1]+waypoint_offset[0])]
     if ship_movement[i][0] == 'L':
-        # print('ship:', ship_position)
-        # print('waypoint:', waypoint_position)
         if ship_movement[i][1] == 90:
             waypoint_offset = [waypoint_position[0]-ship_position[0], waypoint_position[1]-ship_position[1]]
             waypoint_position = [(ship_position[0]-waypoint_offset[1]), (ship_position[1]+waypoint_offset[0])]
@@ -223,17 +165,11 @@ for i in range(0, len(ship_movement)):
         if ship_movement[i][1] == 270:
             waypoint_offset = [waypoint_position[0]-ship_position[0], waypoint_position[1]-ship_position[1]]
             waypoint_position = [(ship_position[0]+waypoint_offset[1]), (ship_position[1]-waypoint_offset[0])]
-        # print('ship:', ship_position)
-        # print('waypoint:', waypoint_position)
     if ship_movement[i][0] == 'F':
-        for x in range(1, ship_movement[i][1]):
+        for x in range(0, ship_movement[i][1]):
             waypoint_offset = [waypoint_position[0] - ship_position[0], waypoint_position[1] - ship_position[1]]
-            # print('ship prior:',ship_position)
-            # print('offset',waypoint_offset)
             ship_position = [waypoint_position[0], waypoint_position[1], ship_position[2]]
-            # print('ship after:',ship_position)
             waypoint_position = [ship_position[0] + waypoint_offset[0], ship_position[1] + waypoint_offset[1]]
-            # print('new waypoint',waypoint_position)
     if ship_movement[i][0] == 'N':
         waypoint_position = [waypoint_position[0], waypoint_position[1] + ship_movement[i][1]]
     if ship_movement[i][0] == 'S':
@@ -242,9 +178,8 @@ for i in range(0, len(ship_movement)):
         waypoint_position = [waypoint_position[0] + ship_movement[i][1], waypoint_position[1]]
     if ship_movement[i][0] == 'W':
         waypoint_position = [waypoint_position[0] - ship_movement[i][1], waypoint_position[1]]
-    turned = 0
 
-print('\n')
+print('\nPart 2 Solution:\n=================================')
 print('Ship Position:', ship_position)
 print('Waypoint Position:', waypoint_position)
-print('Manhattan Distance:', abs(int(waypoint_position[0])) + abs(int(waypoint_position[1])))
+print('Manhattan Distance:', abs(ship_position[0]) + abs(ship_position[1]))
