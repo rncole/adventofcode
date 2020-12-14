71feeb7b74e77f14990c9b7ebd663df7719987e5
Robert Cole <rncole@gmail.com>
2020-12-13 20:08:51 -0500

    Day 13 Part 1

--- 13/main.py
+++ 13/main.py
@@ -1,1 +1,39 @@
 # Advent of Code Day 13: Shuttle Search
+
+infile = open('input.txt','r')
+
+input = []
+with infile as f:
+    for line in f:
+        line = line.rstrip()
+        input.append(line)
+
+t_initial = int(input[0])
+t = t_initial
+bus_routes_tmp = input[1]
+bus_routes = list()
+while len(bus_routes_tmp)>0:
+    bus_routes.append(bus_routes_tmp.partition(',')[0])
+    bus_routes_tmp = bus_routes_tmp.partition(',')[2]
+
+print('time =',t)
+print('Bus Routes = ',bus_routes)
+# print(len(bus_routes))
+earliest_route = ''
+
+i = 0
+t = t_initial
+# print(len(bus_routes))
+while earliest_route == '':
+    for i in range(0, len(bus_routes)):
+        if bus_routes[i] != 'x':
+            test_div = divmod(t, int(bus_routes[i]))[1]
+            if test_div == 0:
+                earliest_route = bus_routes[i]
+                earliest_time = t
+        i = i + 1
+    t = t + 1
+wait_time = earliest_time-t_initial
+print('Earliest Route is:',earliest_route,'at time',earliest_time)
+print('Waiting time:',wait_time)
+print('Solution:',wait_time*int(earliest_route))
